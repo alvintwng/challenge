4ec55086b78f6ab98cab68fb297444ff38ffcea3
AlvinTWNg <alvintwng@gmail.com>
2021-06-03 13:12:06 +0800

    dateTest revised with assertEquals

--- problem2/src/test/java/problem2/dateTest.java
+++ problem2/src/test/java/problem2/dateTest.java
@@ -1,5 +1,7 @@
 package problem2;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -9,20 +11,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class dateTest {
-	@Disabled @Test
+	//@Disabled 
+	@Test
 	void contextLoads() {
 		
-	    String text = "26/4/2021 13:00"; // text in dTF pattern
-	    DateTimeFormatter dTF;  //Declare dateTime formatter
+	    String text = "26/4/2021 13:00"; 					// text in dTF pattern
+	    DateTimeFormatter dTF;  						// Declare dateTime formatter
 	    dTF = DateTimeFormatter.ofPattern("d/M/yyyy HH:mm");
 	    LocalDateTime parsedDate = LocalDateTime.parse(text, dTF);
-	    System.out.println("parsedDate:" + parsedDate);		// parsedDate:2021-04-26T09:00
+	    System.out.println("parsedDate:" + parsedDate);		// parsedDate:2021-04-26T13:00
 	    
 	    DateTimeFormatter tF = DateTimeFormatter.ofPattern("hh:mm a");	// Declare Time format
-	    System.out.println("Time:" + parsedDate.format(tF)); 	// Time:09:00 am
+	    assertEquals( parsedDate.format(tF), "01:00 pm");
 	    
 	    parsedDate = parsedDate.plusMinutes(240);
-	    System.out.println("Time:" + parsedDate.format(tF));		// Time:09:45 am
+	    assertEquals( parsedDate.format(tF), "05:00 pm");
 
 	}
 
