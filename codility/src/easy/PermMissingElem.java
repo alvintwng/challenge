bb8aa8b6e6939bead8b1bdabcad496128fc7965e
AlvinTWNg <alvintwng@gmail.com>
2022-05-03 23:18:00 +0800

    CodeCheck Report: 100%

--- codility/src/easy/PermMissingElem.java
+++ codility/src/easy/PermMissingElem.java
@@ -1,5 +1,5 @@
 /** 220417
- * Codility Lesson 3 Time Complexity. 
+ * Codility Lesson 3 Time Complexity.
  * PermMissingElem
  * Find the missing element in a given permutation.
  */
@@ -29,40 +29,31 @@ public class PermMissingElem {
             return 1;
         }
 
-        int search = 0, lgth = A.length;
+        int lgth = A.length;
+        int Blgth = lgth + 2;
+        byte[] B = new byte[Blgth];
 
-        boolean tryAgain = true;
-
-        doWhile:
-        while (tryAgain) {
-            search = search + 1;
-
-            for (int i = 0; i < lgth; i++) {
-                if (A[i] == search) {
-                    continue doWhile;
-                }
-            }
-
-            tryAgain = false;
-            return search;
+        for (int i = 0; i < lgth; i++) {
+            B[A[i]] = 1;
         }
 
-        return search;
+        for (int i = 1; i <= Blgth; i++) {
+            if (B[i] == 0) {
+                return i;
+            }
+        }
 
+        return 0;
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        
+
         PermMissingElem c = new PermMissingElem();
         int[] A = {2, 3, 1, 5};
         System.out.println("solution A: " + c.solution(A));
-        int[] B = {2, 2, 3, 5, 4};
-        System.out.println("solution B: " + c.solution(B));
-        int[] C = {9, 8, 7, 6, 5, 4, 3}; // first or last
-        System.out.println("solution C: " + c.solution(C));
         int[] D = {2};          // single digit
         System.out.println("solution D: " + c.solution(D));
         int[] E = {};          // single digit
@@ -72,20 +63,45 @@ public class PermMissingElem {
 /* sample Dialogue
 run:
 solution A: 4
-solution B: 1
-solution C: 1
 solution D: 1
 solution E: 1
 BUILD SUCCESSFUL (total time: 0 seconds)
- */
- /* CodeCheck Report: 60% 
-medium2 
-medium test, length = ~10,000
-1. 0.668 s TIMEOUT ERROR,  running time: 0.668 sec., time limit: 0.112 sec.
 
-large_range 
-range sequence, length = ~100,000
-1. 6.000 s TIMEOUT ERROR,  Killed. Hard limit reached: 6.000 sec.
-2. 0.124 s OK
-3. 1.104 s TIMEOUT ERROR,  running time: 1.104 sec., time limit: 0.496 sec.
+ */
+ /* CodeCheck Report: 100% 
+nalysis
+Detected time complexity:
+O(N) or O(N * log(N))
+expand allExample tests
+▶example
+example test✔OK
+expand allCorrectness tests
+▶empty_and_single
+empty list and single element✔OK
+▶missing_first_or_last
+the first or the last element is missing✔OK
+▶single
+single element✔OK
+▶double
+two elements✔OK
+▶simple
+simple test✔OK
+collapse allPerformance tests
+▶medium1
+medium test, length = ~10,000✔OK
+1.0.024 sOK
+▶medium2
+medium test, length = ~10,000✔OK
+1.0.028 sOK
+▶large_range
+range sequence, length = ~100,000✔OK
+1.0.244 sOK
+2.0.124 sOK
+3.0.124 sOK
+▶large1
+large test, length = ~100,000✔OK
+1.0.244 sOK
+▶large2
+large test, length = ~100,000✔OK
+1.0.148 sOK
  */
