203487d32e95a392be499a2003fa49136931a0c4
AlvinTWNg <alvintwng@gmail.com>
2022-05-03 23:29:49 +0800

    question only

---
+++ codility/src/easy/Distinct.java
@@ -0,0 +1,34 @@
+/** 220503
+ * Codility Lesson 6 Sorting
+ * Write a function
+
+class Solution { public int solution(int[] A); }
+
+that, given an array A consisting of N integers, returns the number of distinct values in array A.
+
+For example, given array A consisting of six elements such that:
+
+ A[0] = 2    A[1] = 1    A[2] = 1
+ A[3] = 2    A[4] = 3    A[5] = 1
+the function should return 3, because there are 3 distinct values appearing in array A, namely 1, 2 and 3.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [0..100,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000].
+ */
+package easy;
+
+/**
+ *
+ * @author antw
+ */
+public class Distinct {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+    }
+}
